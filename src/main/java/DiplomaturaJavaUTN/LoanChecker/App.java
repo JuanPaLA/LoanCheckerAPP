bdc13dccb6a70adaa7d046dac7082942a7925a4c
JuanPaLA <juanlopezalurralde@gmail.com>
2018-11-30 12:29:24 -0300

    Commit 30/11 12:29

--- src/main/java/DiplomaturaJavaUTN/LoanChecker/App.java
+++ src/main/java/DiplomaturaJavaUTN/LoanChecker/App.java
@@ -3,44 +3,48 @@ package DiplomaturaJavaUTN.LoanChecker;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.mysql.jdbc.PreparedStatement;
+
 import web.loanchecker.DataBase.DAOCliente;
-import web.loanchecker.DataBase.DAOPagos;
-import web.loanchecker.DataBase.DAOPrestamos;
 import web.loanchecker.model.Clientes;
-import web.loanchecker.model.Pagos;
-import web.loanchecker.model.Prestamos;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ){
-        
+    public static void main( String[] args ) {        
+    	
+    	try {
+    		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3308/loanchecker", "root", "");
+    		Statement pst = null;
+    		String query = "INSERT INTO clientes VALUES (5, 'cacho', 'lopes', 22, '123321', 123123, '123123', 'asdsad', 'asdasd', 'sdfsfs')";
+    		String query2 = "select * from clientes";
+    		
+    		pst =con.createStatement();
+    		
+    		pst.executeUpdate(query);
+   
+    		
+    	}catch(SQLException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	
+    	
+    	
     	//DAOCliente a = new DAOCliente();
-    	
-    	//Clientes nabo = new Clientes(3, "pacho", "herrera", 23, 32165444, "malabai 22", 65414787); 
-    	
-    	DAOPagos p = new DAOPagos();
-    	Pagos pp = new Pagos(1,1,1, "1995-11-11", 4321, 1000, false);
-    	p.insertar(pp);
-    	
-    	//Prestamos p = new Prestamos(1,3,1, 44, 1231, 1000, 12, 11, 10, "1995-12-12", false);
-    	
-    	//DAOPrestamos prest = new DAOPrestamos();
+//    	
+//    	Clientes nabo = new Clientes(3, "pacho", "herrera", 23, 32165444, "malabai 22", 65414787); 
+//    	
+//    	//a.insertar(nabo);
+//    	
+//    	a.eliminar(nabo);
     	
     	
-    	
-    	
-    	
-    	
-    	//prest.insertar(p);
-    	
-    	//a.insertar(nabo);
-    	
-    	//a.eliminar(nabo);
     	
     	
     	
