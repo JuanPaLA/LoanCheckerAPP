73cbb23bfaaaff74350c3a01814d5059fe99a988
JuanPaLA <juanlopezalurralde@gmail.com>
2018-12-02 15:11:46 -0300

    Cambios en clase de branch 2

--- src/main/java/DiplomaturaJavaUTN/LoanChecker/PruebaMasterB.java
+++ src/main/java/DiplomaturaJavaUTN/LoanChecker/PruebaMasterB.java
@@ -6,6 +6,7 @@ public class PruebaMasterB {
 		// TODO Auto-generated method stub
 		System.out.println("PROBANDO BRANCHES");
 		System.out.println("the same");
+		System.out.println("the same");
 	}
 
 }
