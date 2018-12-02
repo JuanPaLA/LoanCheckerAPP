0f3c40cde6b10402786f2b4321f7121716c1be4d
JuanPaLA <juanlopezalurralde@gmail.com>
2018-12-02 14:44:03 -0300

    Probando cambios en second master

--- src/main/java/DiplomaturaJavaUTN/LoanChecker/PruebaMasterB.java
+++ src/main/java/DiplomaturaJavaUTN/LoanChecker/PruebaMasterB.java
@@ -5,6 +5,7 @@ public class PruebaMasterB {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("PROBANDO BRANCHES");
+		System.out.println("the same");
 	}
 
 }
